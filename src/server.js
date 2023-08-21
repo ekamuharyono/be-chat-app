0deb8383546754f443ea78cfca22dfb3e37970fe
ekamuharyono <ekamuharyono02@gmail.com>
2023-08-21 13:23:56 +0700

    123

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const dotenv = require('dotenv').config()
+const PORT = process.env.PORT || 3000;
 const session = require('express-session');
 const bodyParser = require('body-parser');
 const chatRoutes = require('./routes/chat');
